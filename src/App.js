deb60ed1bbafe63f8a0520cdbb2c5dd086714188
Saikumar.Pragallapati <Saikumar.Pragallapati@ibm.com>
2019-02-26 11:44:10 +0530

    layout, graphs

--- src/App.js
+++ src/App.js
@@ -1,170 +1,221 @@
 import React, { Component } from 'react';
 import './App.css';
-import Dropzone from 'react-dropzone'
-import classNames from 'classnames'
-import csvjson from 'csvjson'
-import {ProgressBar} from 'react-bootstrap'
+import Dropzone from 'react-dropzone';
+import classNames from 'classnames';
+import csvjson from 'csvjson';
+import { ProgressBar, Button } from 'react-bootstrap';
 import BootstrapTable from 'react-bootstrap-table-next';
-import {barChart} from './graphs'
+import { barLineChart } from './graphs';
+import Highcharts from 'highcharts';
 
 import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
 
 class App extends Component {
-  constructor(){
-    super()
+  constructor() {
+    super();
     this.state = {
       data: [],
       fileNames: [],
-      now: 0
-    }
+      now: 0,
+    };
   }
-  onDrop=(files)=> {
+  onDrop = files => {
     files.forEach(file => {
-      const reader = new FileReader()
-      let fileName = file.name
+      const reader = new FileReader();
+      let fileName = file.name;
       reader.onload = () => {
         var jsonArray = csvjson.toObject(reader.result, {});
-        this.setState((prevState, props)=>{
+        this.setState((prevState, props) => {
           prevState.fileNames.push(fileName);
           return {
             data: prevState.data.concat(jsonArray),
-            fileNames: prevState.fileNames
-          }
-        })
-      }
+            fileNames: prevState.fileNames,
+          };
+        });
+      };
       reader.readAsBinaryString(file);
     });
-    for (let i=20; i<=100; i+=20){
+    for (let i = 20; i <= 100; i += 20) {
       this.setState({
-        now: i
-      })
+        now: i,
+      });
     }
-  }
-  getColumns=()=>{
+  };
+  getColumns = () => {
     let keys = Object.keys(this.state.data[0]);
-    let columns = []
-    for(let i=0; i<keys.length; i++){
-      let temp = {}
-      temp.dataField = keys[i]
+    let columns = [];
+    for (let i = 0; i < keys.length; i++) {
+      let temp = {};
+      temp.dataField = keys[i];
       temp.text = keys[i].charAt(0).toUpperCase() + keys[i].slice(1);
       temp.sort = true;
       columns.push(temp);
     }
     this.setState({
       columns,
-      fields: keys
-    })
-  }
-  onSubmit=()=>{
+      fields: keys,
+    });
+  };
+  onSubmit = () => {
     this.setState({
-      isSubmit: true
-    })
+      isSubmit: true,
+    });
     this.getColumns();
-  }
-  selectFields=(e)=>{
-    if(e.target.value){
+  };
+  selectFields = e => {
+    if (e.target.value) {
       this.setState({
-        [e.target.name]: e.target.value
-      })
+        [e.target.name]: e.target.value,
+      });
     }
-  }
-  viewChart=(e)=>{
+  };
+  viewChart = e => {
     let chartType = e.target.value;
-    let {xaxis, yaxis} = this.state;
-    if(xaxis && yaxis && chartType){  
-      this.setState({
-        isGraphVisible: true
-      }, () => {
-        barChart(xaxis, yaxis, this.state.data);
-      })
-    }else{
-      alert("Select Both X-Axis and Y-Axis")
+    let { xaxis, yaxis } = this.state;
+    if (xaxis && yaxis && chartType) {
+      this.setState(
+        {
+          isGraphVisible: true,
+        },
+        () => {
+          let res;
+          if (['bar', 'line', 'area', 'pie'].includes(chartType)) {
+            res = barLineChart(xaxis, yaxis, this.state.data, chartType);
+          }
+          if (res) {
+            this.setState({
+              isGraphVisible: false,
+            });
+          }
+        },
+      );
+    } else {
+      alert('Select Both X-Axis and Y-Axis');
     }
-  }
+  };
+
+  close = () => {
+    if (this.state.isGraphVisible) {
+      this.setState(
+        {
+          isSubmit: false,
+        },
+        () => {
+          this.setState({
+            isSubmit: true,
+            isGraphVisible: false,
+          });
+        },
+      );
+    }
+  };
+
   render() {
     return (
       <div className="App">
-      {!this.state.isSubmit?
-        <div>
-          <Dropzone
-                onDrop={this.onDrop}
-              >
-                {({ getRootProps, getInputProps, isDragActive }) => {
-                  return (
-                    <div
-                      {...getRootProps()}
-                      className={classNames('dropzone', 'custom-dropzone', { 'dropzone--isActive': isDragActive })}
-                    >
-                      <input {...getInputProps()} />
-                      {
-                        isDragActive ?
-                          <p>Drop files here...</p> :
-                          <div>
-                            <p>Drap & Drop</p>
-                            <p> or </p>
-                            <p>Browse Files Here</p>
-                          </div>
-                      }
+        {!this.state.isSubmit ? (
+          <div className="upload-page">
+            <span>Upload File</span>
+            <Dropzone onDrop={this.onDrop}>
+              {({ getRootProps, getInputProps, isDragActive }) => {
+                return (
+                  <div
+                    {...getRootProps()}
+                    className={classNames('dropzone', 'custom-dropzone', {
+                      'dropzone--isActive': isDragActive,
+                    })}
+                  >
+                    <input {...getInputProps()} />
+                    <div className="dropzone-message">
+                      <i className="fa fa-hand-o-up" />
+                      <p>Drap & Drop</p>
+                      <p> or </p>
+                      <p>Browse Files Here</p>
                     </div>
-                  )
-                }}
-              </Dropzone>
-          {this.state.now!=0?
-            <ProgressBar variant="success" now={this.state.now} label={`${this.state.now}%`} />
-          :null}
-          {this.state.fileNames.map((e,i)=><div key={i}>{e}</div>)}
-          {this.state.now==100?
-            <button onClick={this.onSubmit}>Submit</button>
-            :null
-          }
-        </div>
-      :
-        <div className="table-graph">
-          <BootstrapTable 
-            keyField={this.state.fields[0]}
-            data={this.state.data} 
-            columns={this.state.columns}
-            striped
-            hover
-            // pagination={ paginationFactory() }
-          />
-          <div>
-            {!this.state.isGraphVisible?
-              <div>
-                <select onChange={this.selectFields} name="xaxis">
-                  <option value="">
-                      X-Axis
-                  </option>
-                  {this.state.fields.map(e=>
-                    <option value={e}>{e}</option>
-                  )}
-                </select>
-                <select onChange={this.selectFields} name="yaxis">
-                  <option value="">
-                    Y-Axis
-                  </option>
-                  {this.state.fields.map(e=>
-                    <option value={e}>{e}</option>
-                  )}
-                </select>
-                <select onChange={this.viewChart}>
-                  <option value="">
-                    Chart Type
-                  </option>
-                  <option value="bar">
-                    Bar Chart
-                  </option>
-              </select>
-              </div>
-            :
-              <div id="container"></div>
-            }
-
-            {JSON.stringify(this.state.fields)}
+                  </div>
+                );
+              }}
+            </Dropzone>
+            <br />
+            {this.state.now !== 0 ? (
+              <ProgressBar
+                variant="success"
+                now={this.state.now}
+                label={`${this.state.now}%`}
+              />
+            ) : null}
+            {this.state.fileNames.map((e, i) => (
+              <div key={i}>{e}</div>
+            ))}
+            {this.state.now === 100 ? (
+              <button onClick={this.onSubmit}>Submit</button>
+            ) : null}
           </div>
-        </div>
-      }
-            
+        ) : (
+          <div>
+            <div
+              className="button-div"
+              onClick={() => {
+                window.location.reload();
+              }}
+            >
+              <b>Choose Another File</b>
+            </div>
+            <div className="table-graph">
+              <BootstrapTable
+                keyField={this.state.fields[0]}
+                data={this.state.data}
+                columns={this.state.columns}
+                striped
+                hover
+              />
+              <div>
+                <span className="close-button" onClick={this.close}>
+                  X
+                </span>
+                {!this.state.isGraphVisible ? (
+                  <div className="selection-container">
+                    <select
+                      onChange={this.selectFields}
+                      name="xaxis"
+                      className="form-control"
+                      value={this.state.xaxis}
+                    >
+                      <option value="">X-Axis</option>
+                      {this.state.fields.map((e, i) => (
+                        <option key={i} value={e}>
+                          {e}
+                        </option>
+                      ))}
+                    </select>
+                    <select
+                      onChange={this.selectFields}
+                      name="yaxis"
+                      className="form-control"
+                      value={this.state.yaxis}
+                    >
+                      <option value="">Y-Axis</option>
+                      {this.state.fields.map((e, i) => (
+                        <option key={i} value={e}>
+                          {e}
+                        </option>
+                      ))}
+                    </select>
+                    <select onChange={this.viewChart} className="form-control">
+                      <option value="">Chart Type</option>
+                      <option value="bar">Bar Chart</option>
+                      <option value="line">Line Chart</option>
+                      <option value="pie">Pie Chart</option>
+                      <option value="area">Area Chart</option>
+                    </select>
+                  </div>
+                ) : (
+                  <div id="container" />
+                )}
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     );
   }
--- src/graphs.js
+++ src/graphs.js
@@ -1,36 +1,54 @@
 import Highcharts from 'highcharts';
+import { pick } from 'lodash';
 
-export function barChart(xaxis, yaxis, data){
-    debugger;
-    var i;
-    for(i = 0; i < data.length; i++){
-        if(xaxis!=='name'){
-            data[i].name = data[i][xaxis];
-            delete data[i][xaxis];
-        }
-        if(yaxis!=='id'){
-            data[i].y = parseInt(data[i][yaxis],10);
-            delete data[i][yaxis];
-        }
+export function barLineChart(xaxis, yaxis, data, chartType) {
+  if (xaxis === yaxis) {
+    alert('Please select different x-axis and y-axis');
+    return true;
+  }
+  data = data.map(item => pick(item, [xaxis, yaxis]));
+  for (var i = 0; i < data.length; i++) {
+    if (xaxis !== 'name') {
+      data[i]['name'] = data[i][xaxis];
+      delete data[i][xaxis];
     }
-    debugger;
-    Highcharts.chart('container', {
-        chart: {
-            type: 'bar'
-        },
-        xAxis: {
-            title: {
-                text: xaxis
-            }
-        },
-        yAxis: {
-            title: {
-                text: yaxis
-            }
-        },
-        series: [{
-            data: [{name: 1, id: 1, }, {name: 2, id: 11, }]
-        }]
-      });
-
+    if (yaxis !== 'y') {
+      if (data[i].y) {
+        data[i]['x'] = data[i].y;
+        delete data[i].y;
+      }
+      if (isNaN(parseInt(data[i][yaxis], 10))) {
+        alert('All y-axis values should be integers to draw bar chart');
+        return true;
+      }
+      data[i].y = parseInt(data[i][yaxis], 10);
+      delete data[i][yaxis];
+    }
+  }
+  Highcharts.chart('container', {
+    chart: {
+      type: chartType,
+    },
+    xAxis: {
+      type: 'category',
+      title: {
+        text: xaxis,
+      },
+    },
+    yAxis: {
+      title: {
+        text: yaxis,
+      },
+    },
+    tooltip: {
+      headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
+      pointFormat:
+        '<span style="color:{point.color}">{point.name}</span>: <b>{point.y}</b> of total<br/>',
+    },
+    series: [
+      {
+        data,
+      },
+    ],
+  });
 }
